4afb978034be1f0dc99fd18c1b6f92af078d219e
Matthew Osborne <momatthewosborne1@gmail.com>
2022-12-22 19:30:13 +0000

    added moment.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -34,27 +34,27 @@ $.ajax({
 
         console.log(response)
 
-        $("#Date2").html("<img src=http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png>");
+        $("#Date2").html(moment().add(1, "days").format("D/MM/YY") + "<img src=http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png>");
         $("#temp2").text("Temperature: " + convertKtoC(response.list[0].main.temp).toFixed(2) + "C");
         $("#wind2").text("Wind: " + response.list[0].wind.speed + " KPH");
         $("#humidity2").text("Humidity: " + response.list[0].main.humidity + "%");
 
-        $("#Date3").html("<img src=http://openweathermap.org/img/w/" + response.list[8].weather[0].icon + ".png>");
+        $("#Date3").html(moment().add(2, "days").format("D/MM/YY") + "<img src=http://openweathermap.org/img/w/" + response.list[8].weather[0].icon + ".png>");
         $("#temp3").text("Temperature: " + convertKtoC(response.list[8].main.temp).toFixed(2) + "C");
         $("#wind3").text("Wind: " + response.list[8].wind.speed + " KPH");
         $("#humidity3").text("Humidity: " + response.list[8].main.humidity + "%");
 
-        $("#Date4").html(response.name + "<img src=http://openweathermap.org/img/w/" + response.list[16].weather[0].icon + ".png>");
+        $("#Date4").html(moment().add(3, "days").format("D/MM/YY") + "<img src=http://openweathermap.org/img/w/" + response.list[16].weather[0].icon + ".png>");
         $("#temp4").text("Temperature: " + convertKtoC(response.list[16].main.temp).toFixed(2) + "C");
         $("#wind4").text("Wind: " + response.list[16].wind.speed + " KPH");
         $("#humidity4").text("Humidity: " + response.list[16].main.humidity + "%");
 
-        $("#Date5").html(response.name + "<img src=http://openweathermap.org/img/w/" + response.list[24].weather[0].icon + ".png>");
+        $("#Date5").html(moment().add(4, "days").format("D/MM/YY") + "<img src=http://openweathermap.org/img/w/" + response.list[24].weather[0].icon + ".png>");
         $("#temp5").text("Temperature: " + convertKtoC(response.list[24].main.temp).toFixed(2) + "C");
         $("#wind5").text("Wind: " + response.list[24].wind.speed + " KPH");
         $("#humidity5").text("Humidity: " + response.list[24].main.humidity + "%");
 
-        $("#Date6").html(response.name + "<img src=http://openweathermap.org/img/w/" + response.list[32].weather[0].icon + ".png>");
+        $("#Date6").html(moment().add(5, "days").format("D/MM/YY") + "<img src=http://openweathermap.org/img/w/" + response.list[32].weather[0].icon + ".png>");
         $("#temp6").text("Temperature: " + convertKtoC(response.list[32].main.temp).toFixed(2) + "C");
         $("#wind6").text("Wind: " + response.list[32].wind.speed + " KPH");
         $("#humidity6").text("Humidity: " + response.list[32].main.humidity + "%");
